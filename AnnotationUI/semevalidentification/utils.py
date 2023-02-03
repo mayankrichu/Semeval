8d3c1e33e2710938288f5ab96e38959d118ae36d
mayankrichu <71438205+mayankrichu@users.noreply.github.com>
2023-02-03 14:44:05 +0100

    Delete AnnotationUI directory

--- AnnotationUI/semevalidentification/utils.py
+++
@@ -1,1 +0,0 @@
-import semevalidentification.routes
--- AnnotationUI/variables.py
+++
@@ -1,8 +0,0 @@
-
-semeval_file_path = "/Volumes/Mayank HDD/Fraunhofer Project/Semeval/Semeval2.csv"
-
-annotated_file_path = "/Volumes/Mayank HDD/Fraunhofer Project/Semeval/Semeval_Annotated/"
-
-True_annotated_file_path = "/Volumes/Mayank HDD/Fraunhofer Project/Semeval/TrueAnnotated/"
-
-False_annotated_file_path = "/Volumes/Mayank HDD/Fraunhofer Project/Semeval/FalseAnnotated/"
